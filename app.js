b048ac020d6c082a860cc0ecc0122d4a4c09b77f
Guilherme Henrique <guihenrique.s@outlook.com>
2025-04-03 13:42:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,11 @@ function sortear() {
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function reiniciar() {
+    document.getElementById('quantidade').value = "";
+    document.getElementById('de').value = "";
+    document.getElementById('ate').value = "";
+    document.getElementById('resultado').innerText = "";
+}
+
